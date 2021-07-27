babe4c8c205862d0f1a8655c469924a19be5b6b8
unknown <zuuareb@gmail.com>
2021-07-26 21:25:14 -0500

    finshed script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // TODO: add code here
+// TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+    return response.json()
+}).then( function(json) {
+            console.log(json);
+            let container=document.getElementById("container")
+            let astronauts =''
+
+        for (let astronaut of json){
+            astronauts+=`<div"astronaut">
+   <div class="bio">
+      <h3> ${astronaut.firstName} ${astronaut.lastName} </h3>
+      <ul>
+         <li>Hours in space: ${astronaut.hoursInSpace}</li>
+         <li>Active: ${astronaut.active}</li>
+         <li>Skills: ${astronaut.skills.join(", ")}, Chemical Engineer</li>
+      </ul>
+   </div>
+   <img src="${astronaut.picture}" alt="" class="avatar" />
+</div>`
+
+         };
+    container.innerHTML =astronauts
+    })
+})
+
